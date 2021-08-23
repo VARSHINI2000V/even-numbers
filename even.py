be009bdebff4f3c7586ec2992c4e3e61e73ce379
ruba1705 <77003507+ruba1705@users.noreply.github.com>
2021-08-23 14:17:05 +0530

    Update even.py
    varshini i made some changes

--- even.py
+++ even.py
@@ -1,9 +1,9 @@
 def even(n):
     print("The even number are.....")
-    for i in range(n):
-        if i%2==0:
-            print(i)
-
+    ans=2
+    while(ans<=n):
+        print(ans)
+        ans=ans+2
 if __name__=="__main__":
     n=int(input("Enter the range in which you want even numbers:"))
     even(n)
